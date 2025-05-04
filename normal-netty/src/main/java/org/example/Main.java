e7b54cb3fed67f6458f33e9998fa55d8ece3ba81
eufrat <lukzyw@gmail.com>
2025-05-05 01:01:40 +0200

    norm vs virt, tomcat netty

--- normal-netty/src/main/java/org/example/Main.java
+++ normal-netty/src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +15,7 @@ import java.nio.file.Path;
 
 @RestController
 @SpringBootApplication
+@Configuration
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
