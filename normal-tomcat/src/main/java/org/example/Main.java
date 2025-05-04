ddf7a00b62636fa27bd3b5b01ffca3fd5201099d
eufrat <lukzyw@gmail.com>
2025-05-05 00:07:33 +0200

    Change to modules

--- normal-tomcat/src/main/java/org/example/Main.java
+++ normal-tomcat/src/main/java/org/example/Main.java
@@ -2,10 +2,22 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@RestController
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class);
+    }
+
+    @GetMapping("/read")
+    public int read() throws IOException {
+        return Files.readAllBytes(Path.of("myfile.bin")).length;
     }
 }
